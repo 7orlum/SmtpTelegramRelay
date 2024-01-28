80c0466065970ebf2031c4e9c52b18cb2bdb2151
7orlum <8972185+7orlum@users.noreply.github.com>
2024-01-28 14:37:01 +0100

    Use appsettings.yaml to configure the application

--- SmtpTelegramRelay/Program.cs
+++ SmtpTelegramRelay/Program.cs
@@ -5,8 +5,14 @@ public static class Program
     static void Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddHostedService<Relay>();
-        builder.Services.Configure<RelayConfiguration>(builder.Configuration);
+        
+        builder.Configuration
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddYamlFile("appsettings.yaml", optional: true)
+            .AddYamlFile($"appsettings.{builder.Environment.EnvironmentName}.yaml", optional: true);
+        builder.Services
+            .AddHostedService<Relay>()
+            .Configure<RelayConfiguration>(builder.Configuration);
 
         var host = builder.Build();
         host.Run();
